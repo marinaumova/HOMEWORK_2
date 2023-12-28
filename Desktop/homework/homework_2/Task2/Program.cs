a399a55eb86267799f1601cacc1d435ddcfa08b3
Marina Naumova <marinaumova@list.ru>
2023-12-28 21:47:58 +0300

    add homework_2:Task2

---
+++ Desktop/homework/homework_2/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write ("Введите координаты точки x и y: ");
+string[] coordinates = Console.ReadLine().Split(' ');
+int x = Convert.ToInt32(coordinates[0]);
+int y = Convert.ToInt32(coordinates[1]);
+if (x > 0 && y > 0)
+{
+Console.WriteLine("1");
+}
+else if (x < 0 && y > 0)
+{
+Console.WriteLine("2");
+}
+else if (x < 0 && y < 0)
+{
+Console.WriteLine("3");
+}
+else if (x > 0 && y < 0)
+{
+Console.WriteLine("4");
+}
