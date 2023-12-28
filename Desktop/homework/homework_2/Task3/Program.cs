6ddde754eb2f4be1d4d863863f93b576886ad1f2
Marina Naumova <marinaumova@list.ru>
2023-12-28 22:00:35 +0300

    add homework_2:Task3

---
+++ Desktop/homework/homework_2/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число от 10 до 99: "); 
+int number = Convert.ToInt32(Console.ReadLine());
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine(firstDigit);
+}
+else
+{
+    Console.WriteLine(secondDigit);
+}
