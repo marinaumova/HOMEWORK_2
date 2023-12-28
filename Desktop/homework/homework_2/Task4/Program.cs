e07437c539ae8b1f7ac34b685b14d8056a68c075
Marina Naumova <marinaumova@list.ru>
2023-12-28 22:31:39 +0300

    add homework_2:Task4

---
+++ Desktop/homework/homework_2/Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите натуральное число N: "); 
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 10) 
+{
+    Console.WriteLine(N); 
+}
+else 
+{
+    while (N > 0) 
+    {
+        int lastDigit = N % 10; 
+        N /= 10;
+        if (N > 0)
+        {
+            Console.Write(lastDigit + ","); 
+        }
+        else 
+        {
+            Console.WriteLine(lastDigit); 
+        }
+    }
+}
+
