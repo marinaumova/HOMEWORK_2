6b8b90cd2a372d1f07dd4d37858126818857fe4a
Marina Naumova <marinaumova@list.ru>
2024-01-19 20:05:08 +0300

    add Homework4:Task1

---
+++ Desktop/homework/Homework4/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
+// Программа завершается при вводе символа ‘q’ или при вводе числа, 
+// сумма цифр которого чётная.
+
+while (true)
+{
+    Console.Write("Введите текст: ");
+    string text = Console.ReadLine();
+
+    if ( text == "q")
+    {
+        break;
+    }
+    int number;
+    if (int.TryParse(text, out number))
+    {
+        int sum = 0;
+        while (number > 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        if (sum % 2 == 0)
+        {
+            Console.WriteLine("STOP");
+            break;
+        }
+    }
+}
