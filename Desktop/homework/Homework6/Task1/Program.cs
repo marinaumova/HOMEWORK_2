30dee5218b553128dec9783b9881e8df9a2e46c9
Marina Naumova <marinaumova@list.ru>
2024-02-02 00:32:33 +0300

    add Homework6:Task1

---
+++ Desktop/homework/Homework6/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿char[,] charArray = new char[,] { { 'a', 'b' }, { 'c', 'd' } };
+string result = GetStringFromCharArray(charArray);
+Console.WriteLine(result);
+static string GetStringFromCharArray(char[,] array)
+{
+    string result = "";
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            result += array[i, j];
+        }
+    }
+    return result;
+}
