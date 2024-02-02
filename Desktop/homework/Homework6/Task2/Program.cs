9063edffd99f8ad5e78785c526d77af499c89c33
Marina Naumova <marinaumova@list.ru>
2024-02-02 13:19:56 +0300

    add Homework6:Task2

---
+++ Desktop/homework/Homework6/Task2/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите строку, содержащую латинские буквы в обоих регистрах:");
+string input = Console.ReadLine();
+string result = input.ToLower();
+Console.WriteLine("Результат: " + result);
