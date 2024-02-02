65bb2401b52bc658774ac9ee4e5f5c8139b4d862
Marina Naumova <marinaumova@list.ru>
2024-02-02 13:34:15 +0300

    add Homework6:Task3

---
+++ Desktop/homework/Homework6/Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите строку: ");
+string input = Console.ReadLine();
+if (IsPalindrome(input))
+     {
+        Console.WriteLine("Строка является палиндромом.");
+     }
+else
+    {
+        Console.WriteLine("Строка не является палиндромом.");
+    }
+bool IsPalindrome(string str)
+{
+    str = str.Replace(" ", "").ToLower();
+char[] charArray = str.ToCharArray();
+Array.Reverse(charArray);
+string reversedString = new string(charArray);
+return str == reversedString;
+}
