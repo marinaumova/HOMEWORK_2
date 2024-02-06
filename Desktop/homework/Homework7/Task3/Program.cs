d2d273f9715f503217e3535012691b3e18c78b54
Marina Naumova <marinaumova@list.ru>
2024-02-06 12:50:49 +0300

    add Homework7:Task3

---
+++ Desktop/homework/Homework7/Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] array = {1, 2, 5, 0, 10, 34};
+int size = array.Length;
+void ShowArray(int[] arr, int size)
+{
+    if (size == 0)
+    {
+        Console.Write(arr[size]);
+        return;
+    }
+    Console.Write(arr[size] + " ");
+    ShowArray(arr, size - 1);
+}
+ShowArray (array, size - 1);
