b351c8f24f9eb4b1c8622c8cd863db30307b45c0
Marina Naumova <marinaumova@list.ru>
2024-02-06 12:11:58 +0300

    add Homework7:Task2

---
+++ Desktop/homework/Homework7/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите неотрицательное число m: "); 
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите неотрицательное число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int Ack(int n, int m)
+{
+    if (n == 0)
+    {
+        return m + 1;
+    }
+    else if (m == 0)
+    {
+        return Ack(n - 1, 1);
+    }
+    else
+    {
+        return Ack(n - 1, Ack(n, m - 1));
+    }
+}
+int result = Ack(n, m);
+Console.Write($"Ack({m}, {n}) = {result}");
