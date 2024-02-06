f464b8452d977039e6dacb60f5c78d2db29cfcd6
Marina Naumova <marinaumova@list.ru>
2024-02-06 11:36:33 +0300

    add Homework7:Task1

---
+++ Desktop/homework/Homework7/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+void ShowNumbers(int start, int end)
+{
+    if (start == end)
+    {
+        Console.Write(start);
+        return;
+    }
+    Console.Write(start + " ");
+    ShowNumbers(start + 1, end);
+}
+ShowNumbers(M, N);
